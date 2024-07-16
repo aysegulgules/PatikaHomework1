cfc948cb3ffeee9a77b0caf64ada6e811f32f9a9
aysegulgules <159700271+aysegulgules@users.noreply.github.com>
2024-07-16 23:05:12 +0300

    comment line

--- PatikaHomework1/Program.cs
+++ PatikaHomework1/Program.cs
@@ -1,2 +1,3 @@
-﻿Console.WriteLine("Ayşegül Güleş");
+﻿//Patike Bootcamp de ilk uygulamam
+Console.WriteLine("Ayşegül Güleş");
 Console.WriteLine("Patika'nın uzman kadrosu ve tecrübeli eğitmenleri sayesinde yazılım bilgilerimi tazelemek ve yeni teknolojileri öğrenmek için bu bootcampteyim!");
